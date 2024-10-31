de3744d65d3965b4a559a78e64af0d470e3ad85b
Dan581 <92440709+Dan581@users.noreply.github.com>
2024-10-31 13:06:28 -0400

    Add files via upload

---
+++ ChinookCamera/main.py
@@ -0,0 +1,8 @@
+# Import the display process
+from display_image import display_video
+
+# set display duration
+display_time = 20
+
+# Call the display_video function directly
+display_video(display_time)
